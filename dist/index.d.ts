7c43fd04c949e8e540db893d4123d250818f30db
VelociRaptor <77036902+zignis@users.noreply.github.com>
2023-02-24 21:35:36 +0530

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { NextFunction, Request, Response } from 'express';
-type Middleware = (req: Request, res: Response, next: NextFunction) => void;
-export declare const deflight: <R extends Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>, M extends Middleware>(middleware: M) => (req: R, res: Response, next: NextFunction) => void;
-export default deflight;
